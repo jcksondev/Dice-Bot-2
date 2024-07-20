47b715877ab9ccb5522d8f17cdab5250d5a42849
JcksonDev <65576130+JcksonDev@users.noreply.github.com>
2024-07-19 22:46:52 -0700

    Added d20 function with modifier and adv/dis support

---
+++ main.py
@@ -0,0 +1,50 @@
+import discord
+from discord.ext import commands
+import random
+import config
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='.', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.display_name} #{bot.user.discriminator}: (ID: {bot.user.id})')
+
+@bot.command()
+async def rr(ctx, *args):
+    arguments = ''.join(args).lower().replace(',', '')
+    rolls = []
+    total = 0
+    modifier = 0
+
+    nat1 = '**that\'s rough buddy..**'
+    nat20 = '**Natural 20!**'
+
+    adv = 'adv' in arguments
+    dis = 'dis' in arguments
+    if '+' in arguments or '-' in arguments:
+        modifier = int(arguments[0:2])
+
+    if adv or dis:
+        for _ in range(2):
+            rolls.append(random.randint(1,20))
+
+        if adv:
+            total = max(rolls) + modifier
+        else: 
+            total = min(rolls) + modifier
+    else:
+        rolls.append(random.randint(1,20))
+        total = rolls[0] + modifier
+
+    rolls = ', '.join(map(str, rolls))
+    message = f'**Rolls**: [ {rolls} ] {arguments[0]} {arguments[1]} \n**Total**: {total}'
+    if total + -modifier == 20: await ctx.send(f'{message}\n{nat20}')
+    elif total + -modifier == 1: await ctx.send(f'{message}\n{nat1}')
+    else: await ctx.send(message)
+
+
+bot.run(config.token)
