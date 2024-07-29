49a3b8f274a83eceb4c70624a13327b66fc443eb
JcksonDev <65576130+JcksonDev@users.noreply.github.com>
2024-07-28 20:37:38 -0700

    Added logging

--- main.py
+++ main.py
@@ -3,48 +3,52 @@ from discord.ext import commands
 import random
 import settings
 
-intents = discord.Intents.default()
-intents.members = True
-intents.message_content = True
+logger = settings.logging.getLogger('bot')
 
-bot = commands.Bot(command_prefix='.', intents=intents)
+def run():
+    intents = discord.Intents.default()
+    intents.members = True
+    intents.message_content = True
 
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user.display_name} #{bot.user.discriminator}: (ID: {bot.user.id})')
+    bot = commands.Bot(command_prefix='.', intents=intents)
+    @bot.event
+    async def on_ready():
+        logger.info(f'Logged in as {bot.user.display_name} #{bot.user.discriminator}: (ID: {bot.user.id})')
 
-@bot.command()
-async def rr(ctx, *args):
-    arguments = ''.join(args).lower().replace(',', '')
-    rolls = []
-    total = 0
-    modifier = 0
+    @bot.command()
+    async def rr(ctx, *args):
+        arguments = ''.join(args).lower().replace(',', '')
+        rolls = []
+        total = 0
+        modifier = 0
 
-    nat1 = '**that\'s rough buddy..**'
-    nat20 = '**Natural 20!**'
+        nat1 = '**that\'s rough buddy..**'
+        nat20 = '**Natural 20!**'
 
-    adv = 'adv' in arguments
-    dis = 'dis' in arguments
-    if '+' in arguments or '-' in arguments:
-        modifier = int(arguments[0:2])
+        adv = 'adv' in arguments
+        dis = 'dis' in arguments
+        if '+' in arguments or '-' in arguments:
+            modifier = int(arguments[0:2])
 
-    if adv or dis:
-        for _ in range(2):
+        if adv or dis:
+            for _ in range(2):
+                rolls.append(random.randint(1,20))
+
+            if adv:
+                total = max(rolls) + modifier
+            else: 
+                total = min(rolls) + modifier
+        else:
             rolls.append(random.randint(1,20))
+            total = rolls[0] + modifier
 
-        if adv:
-            total = max(rolls) + modifier
-        else: 
-            total = min(rolls) + modifier
-    else:
-        rolls.append(random.randint(1,20))
-        total = rolls[0] + modifier
+        rolls = ', '.join(map(str, rolls))
+        message = f'**Rolls**: [ {rolls} ] {arguments[0]} {arguments[1]} \n**Total**: {total}'
+        if total + -modifier == 20: await ctx.send(f'{message}\n{nat20}')
+        elif total + -modifier == 1: await ctx.send(f'{message}\n{nat1}')
+        else: await ctx.send(message)
 
-    rolls = ', '.join(map(str, rolls))
-    message = f'**Rolls**: [ {rolls} ] {arguments[0]} {arguments[1]} \n**Total**: {total}'
-    if total + -modifier == 20: await ctx.send(f'{message}\n{nat20}')
-    elif total + -modifier == 1: await ctx.send(f'{message}\n{nat1}')
-    else: await ctx.send(message)
+    bot.run(settings.TOKEN, root_logger=True)
 
-
-bot.run(settings.TOKEN)
+if __name__ == "__main__":
+    run()
--- settings.py
+++ settings.py
@@ -1,6 +1,37 @@
+import logging
 import os
+from logging.config import dictConfig
 from dotenv import load_dotenv
 
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
+
+LOGGING_CONFIG = {
+    'version': 1,
+    'disabled_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)-s - %(asctime)s - %(module)-s : %(message)s'
+        },
+        'standard': {
+            'format': '%(levelname)-s - %(module)-s : %(message)s'
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'INFO',
+            'formatter': 'verbose',
+        }
+    },
+    'loggers': {
+        'bot': {
+            'level': 'INFO',
+            'propagate': False,
+            'handlers': ['console']
+        }
+    }
+}
+
+dictConfig(LOGGING_CONFIG)
