06427faaf44d0a0586cc36240f32f4b0eeb9ffc1
JcksonDev <65576130+JcksonDev@users.noreply.github.com>
2024-10-15 14:15:25 -0700

    Add logging

--- setup.py
+++ setup.py
@@ -1,6 +1,41 @@
 import os
+import logging
+from logging.config import dictConfig
+import pathlib
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
+
+BASE_DIR = pathlib.Path(__file__).parent
+
+CMDS_DIR = BASE_DIR / "src/cmds"
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "standard": {
+            "format": "%(levelname)s : %(message)s - %(asctime)s",
+            "datefmt": "%Y-%m-%d %H:%M:%S"
+        }
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "level": "DEBUG",
+            "formatter": "standard"
+        }
+    },
+    "loggers": {
+        "bot": {
+            "level": "INFO",
+            "propagate": False,
+            "handlers": ['console']
+        }
+    },
+}
+
+dictConfig(LOGGING_CONFIG)
--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,8 @@ import discord
 from discord.ext import commands
 
 def main():
+    logger = setup.logging.getLogger('bot')
+
     intents = discord.Intents.default()
     intents.message_content = True
     intents.members = True
@@ -11,9 +13,9 @@ def main():
 
     @bot.event
     async def on_ready():
-        print(f'We have logged in as {bot.user.name}')
+        logger.info(f"Logged in as {bot.user.name} #{bot.user.discriminator}")
 
-    bot.run(setup.TOKEN)
+    bot.run(setup.TOKEN, root_logger=True)
 
 
 if __name__ == "__main__":
