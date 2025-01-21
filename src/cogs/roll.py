d83fff276756d466914ce7b3040239ab160f3aeb
JcksonDev <65576130+JcksonDev@users.noreply.github.com>
2025-01-21 14:50:22 -0800

    Add dice roll functionality

--- src/cogs/roll.py
+++ src/cogs/roll.py
@@ -1,13 +1,56 @@
 import discord
 from discord.ext import commands
+import random
+
 
 class Roll(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.rolls = []
+        self.total = 0
+        self.modifier = 0
+
+    def parse(self, args: tuple):
+        return "".join(args).replace("+", " + ").replace("-", " - ").split()
+
+    def output(self):
+        if self.modifier == 0:
+            return f"**Rolls:** [ {', '.join(self.rolls)} ]\n**Total:** {self.total}"
+        else:
+            return f"**Rolls:** [ {', '.join(self.rolls)} ] {self.modifier:=+4}\n**Total:** {self.total}"
+
+    def reset(self):
+        self.rolls = []
+        self.total = 0
+        self.modifier = 0
 
     @commands.command()
-    async def hello(self, ctx):
-        await ctx.send(f"Hello {ctx.author}")
+    async def r(self, ctx, *args):
+        input = self.parse(args)
+
+        for i, arg in enumerate(input):
+            if "d" in arg:
+                arg = arg.split("d")
+                if arg[0] == "":
+                    arg[0] = 1
+
+                for _ in range(int(arg[0])):
+                    roll = random.randint(1, int(arg[1]))
+                    if roll == int(arg[1]):
+                        self.rolls.append(f"**{str(roll)}**")
+                    else:
+                        self.rolls.append(str(roll))
+                    self.total = self.total + roll
+            elif "+" in arg or "-" in arg:
+                continue
+            else:
+                roll = int("".join([input[i - 1], input[i]]))
+                self.modifier = roll
+                self.total = self.total + roll
+
+        await ctx.send(self.output())
+        self.reset()
+
 
 async def setup(bot):
     await bot.add_cog(Roll(bot))
--- src/setup.py
+++ src/setup.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
 
-BASE_DIR = pathlib.Path(__file__).parent
+BASE_DIR = pathlib.Path(__file__).parent.parent
 SRC_DIR = BASE_DIR / "src"
 COGS_DIR = SRC_DIR / "cogs"
 
@@ -19,22 +19,18 @@ LOGGING_CONFIG = {
     "formatters": {
         "standard": {
             "format": "%(levelname)s : %(message)s - %(asctime)s",
-            "datefmt": "%Y-%m-%d %H:%M:%S"
+            "datefmt": "%Y-%m-%d %H:%M:%S",
         }
     },
     "handlers": {
         "console": {
             "class": "logging.StreamHandler",
             "level": "DEBUG",
-            "formatter": "standard"
+            "formatter": "standard",
         }
     },
     "loggers": {
-        "bot": {
-            "level": "INFO",
-            "propagate": False,
-            "handlers": ['console']
-        }
+        "bot": {"level": "INFO", "propagate": False, "handlers": ["console"]}
     },
 }
 
